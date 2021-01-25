7a0020717b48b2fead3a1d6d88148110e8825d81
arron <aaronliang@trussan.com>
2021-01-25 12:01:48 +0800

    feat: add API doc

--- libs/update.js
+++ libs/update.js
@@ -1,17 +1,20 @@
 const axios = require('axios')
 const compareVersions = require('compare-versions')
+const color = require('cli-color')
+const terminalLink = require('terminal-link')
 
 module.exports = async (v) => {
-    // 拿到所有的 Node 版本
-    const { data } = await axios.get('https://npm.taobao.org/mirrors/node/index.json')
+// 拿到所有的 Node 版本
+const { data } = await axios.get('https://npm.taobao.org/mirrors/node/index.json')
 
-    // 把目标版本的 LTS 都挑选出来
-    return data.filter(node => {
-        const cp = v ? (compareVersions(node.version, 'v' + v + '.0.0') >= 0) : true
-        return node.lts && cp
-    }).map(it => {
-        // 踢出去 file 这个字段， 其他的全部返回
-        const { files, ...rest } = it
-        return { ...rest }
-    })
+// 把目标版本的 LTS 都挑选出来
+return data.filter(node => {
+const cp = v ? (compareVersions(node.version, 'v' + v + '.0.0') >= 0) : true
+return node.lts && cp
+}).map(it => {
+// 踢出去 file 这个字段， 其他的全部返回
+const { files, ...rest } = it
+const doc = color.yellow(terminalLink('API', `https://npm.taobao.org/mirrors/node/${it.version}/docs/api/documentation.html`))
+return { ...rest, doc}
+})
 }
